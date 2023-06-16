77c7a9de2c92dfada176a04141446225cc294ce8
Hansennico <125004991+Hansennico@users.noreply.github.com>
2023-06-16 18:53:09 +0700

    Add files via upload

---
+++ script/cafe.js
@@ -0,0 +1,36 @@
+let products = [
+    {
+        "name": "Cold Brew",
+        "price": 24000,
+        "image": "./asset/coldbrew.jpg"
+    },
+    {
+        "name": "Cold Brew",
+        "price": 24000,
+        "image": "./asset/coldbrew.jpg"
+    },
+    {
+        "name": "Cold Brew",
+        "price": 24000,
+        "image": "./asset/coldbrew.jpg"
+    },
+    {
+        "name": "Cold Brew",
+        "price": 24000,
+        "image": "./asset/coldbrew.jpg"
+    }
+]
+
+let container = document.querySelector(".container")
+for (let i = 0; i < products.length; i++) {
+    container.innerHTML += `
+        <div class="card">
+            <div class="card-text">
+                <h3>${products[i].name}</h3>
+                <img src="${products[i].image}" alt="" class="card-img">
+                <p>Rp. ${products[i].price},-</p>
+                <button type="button" onclick="" class="cart-btn">Add to cart!</button>
+            </div>
+        </div>
+    `
+}
